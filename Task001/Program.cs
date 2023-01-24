18aa5a426a1fffd30f525c1f6b9267d3e56b0fbf
Koka <kshurgaya91@gmail.com>
2023-01-25 00:58:33 +0300

    task1

---
+++ Task001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+// Не использовать строки для расчета.
+
+int InputInt(string message)
+{
+    System.Console.WriteLine(message + " >");
+    string num = Console.ReadLine();
+    int result = Convert.ToInt32(num);
+    return result;
+}
+
+bool ValidateNumberRank3(int number)
+{
+    if (number > 99 && number < 1000)
+    {
+        int secondRank = number / 10 % 10;
+        System.Console.WriteLine($"Вторая цифра числа {number} равна {secondRank}");
+        return true;
+    }
+    else
+    {
+        System.Console.WriteLine("Число не трехзначное");
+        return false;
+    }
+}
+int number = InputInt("Введите число");
+if (ValidateNumberRank3(number));
