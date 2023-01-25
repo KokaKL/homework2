0b8cb5e2ee1f0060a48542d49a119348db64d2b9
Koka <kshurgaya91@gmail.com>
2023-01-26 00:12:22 +0300

    task003

---
+++ Task003/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int dayInput (string massage)
+{
+    System.Console.WriteLine(massage);
+    string day = Console.ReadLine();
+    int result = Convert.ToInt32(day);
+    return result;
+}
+
+int dayOFweek(int days)
+{
+    if (days>0 && days<7 )
+    {
+        string[] arrayweek = new string[7] {"ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"};
+        System.Console.WriteLine(arrayweek[days-1]);
+    }
+    else
+    {
+        System.Console.WriteLine("это не день недели");
+    }
+    return days;
+} 
+
+int inputday =dayInput("Введите номер дня недели");
+int resultday = dayOFweek(inputday);
